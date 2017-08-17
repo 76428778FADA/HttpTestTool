e9216009ae8758ad0c19e37d683d11fdfa841bf7
ZDT@ABC <zhaodongtiao@abchina.com>
2017-08-17 14:10:26 +0800

    Update MessageBoxUtility

--- HttpTestTool/Utilities/MessageBoxUtility.cs
+++ HttpTestTool/Utilities/MessageBoxUtility.cs
@@ -13,7 +13,7 @@ namespace HttpTestTool.Utilities
         {
             var left = Application.Current.MainWindow.Left;
             var width = Application.Current.MainWindow.Width;
-            var top = Application.Current.MainWindow.Top;
+            var top = Application.Current.MainWindow.WindowState == WindowState.Maximized ? -5 : Application.Current.MainWindow.Top;
             int delay = isSuccess ? 1000 : 3000;
             var messageWindow = new Pages.MessageWindow(message, !isSuccess, delay)
             {
